a77f99736a3b274df92d8acfc9f07351e019b789
kisakyegordon <gordon56692@gmail.com>
2017-11-19 20:46:30 +0300

    adding an api connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import 'babel-polyfill';  
 import React from 'react';  
 import { render } from 'react-dom';  
+import configureStore from './store/configureStore';  
+import { Provider } from 'react-redux';  
 import { Router, browserHistory } from 'react-router';  
-import routes from './routes';
+import routes from './routes';  
+import {loadCats} from './actions/catActions';
+
+const store = configureStore();
+
+store.dispatch(loadCats());
 
 render(  
- <Router history={browserHistory} routes={routes} />,
- document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
 );
